48c3982e3e4d4c0d330cbce83ade39236849706c
Mite Stojanov <stojanov1990@gmail.com>
2024-08-02 16:52:14 +0200

    fix: make sure project starts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import './assets/main.css';
 import 'primeicons/primeicons.css';
-import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
+
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import Toast from 'vue-toastification';
+
+import App from './App.vue';
 import router from './router';
 
-import { createApp } from 'vue';
-import App from './App.vue';
-
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
+app.use(pinia);
 app.use(Toast);
 
 app.mount('#app');
